bfc4406ce93a28babf5ab046a2b3ec5b88ffe2ba
ryubek <ryubek91@gmail.com>
2022-01-17 02:28:09 +0600

    Доделываю хвосты. Попробовал немного изменить код, не получилось не хватает знаний. Попробовал новые строки кода, посмотреть как оно.

--- month1lesson1homework/main.swift
+++ month1lesson1homework/main.swift
@@ -7,5 +7,28 @@
 
 import Foundation
 
-print("Hello, World!")
+//var firstName = [String]("Бексултан")
+//
+//var lastName = [String]("Калбаев")
 
+var firstName : String = "Beksultan"
+
+var lastName : String = "Kalbaev"
+
+var yearBirth = 2000
+
+var yearNow = 2022
+
+func birth_card() {
+    let age = yearNow - yearBirth
+    print("\(firstName) \(lastName), \(age) years old ")
+}
+birth_card()
+
+
+//let now = Date()
+//let birthday: Date = 7.15.2000
+//let calendar = Calendar.current
+//
+//let ageComponents = calendar.dateComponents([.year], from: birthday, to: now)
+//let age = ageComponents.year!
